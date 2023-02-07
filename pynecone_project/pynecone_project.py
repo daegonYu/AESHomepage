df3aa844a109403f0e4811b133eb12ae5dc4e439
daegon Yu <ydg5025@naver.com>
2023-02-08 02:42:29 +0900

    버튼이 안됨

--- pynecone_project/pynecone_project.py
+++ pynecone_project/pynecone_project.py
@@ -2,8 +2,9 @@
 from pcconfig import config
 
 import pynecone as pc
+import os 
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
+# docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 # 링크 걸기 예제
@@ -27,8 +28,10 @@ class State(pc.State):      # 함수나 변수 전부 State. 으로 접근한다
     """The app state."""
 
     point_processing = False
+    essay = 3
     
     def get_point(self):
+        # os.system('"ex.py"')
         pass
     
     def process_state(self):        # 버튼을 누르면 True로 변경
@@ -45,10 +48,10 @@ def index():
             pc.box("2) 참신성", font_size="1em"),
             pc.box("3) 설득력", font_size="1em"),
             
-            pc.input(placeholder="Input my text"),
+            pc.input(placeholder="Input my text",on_blur=State.set_essay),  # set_변수명에 저장되는듯?
             pc.button(
                 "나의 글 점수 측정하기",
-                on_click=[State.get_point,State.process_state],
+                on_click=[State.process_state,State.get_point],
                 width="100%",
             ),
             
@@ -58,12 +61,11 @@ def index():
             pc.cond(
                 State.point_processing,     # 버튼 누르면 보임
                 pc.circular_progress(is_indeterminate=True),    # 원형 진행바
-                pc.cond(
-                    True,
-                    pc.box("1) 논리성 : ", font_size="1em"),
-                    pc.box("2) 참신성 : ", font_size="1em"),
-                    pc.box("3) 설득력 : ", font_size="1em"),
-                    
+                pc.cond(        # cond() 하나에 글 하나씩
+                    State.point_processing,
+                    pc.box("1) 논리성 : {}".format(State.essay), font_size="1em"),
+                    # pc.box("2) 참신성 : ", font_size="1em"),
+                    # pc.box("3) 설득력 : ", font_size="1em"),  
                 ),
             ),                    
                 
@@ -74,7 +76,8 @@ def index():
             spacing="1.5em",
             # padding_top="10%"     # 위에 패딩 주기
             ),
-            
+        
+        # 배경
         width="100%",
         height="100vh",
         background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
